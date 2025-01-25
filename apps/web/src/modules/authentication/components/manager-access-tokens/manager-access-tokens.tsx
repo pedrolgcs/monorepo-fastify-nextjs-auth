3384652ce631738d5a9b24fa6376cd04affe329b
Pedro Henrique <pedro.lg.cs@gmail.com>
2025-01-25 11:33:28 -0300

    test: create e2e test from access tokens paginate

--- apps/web/src/modules/authentication/components/manager-access-tokens/manager-access-tokens.tsx
+++ apps/web/src/modules/authentication/components/manager-access-tokens/manager-access-tokens.tsx
@@ -64,7 +64,7 @@ export function ManagerAccessTokens() {
 
   return (
     <div className="space-y-4">
-      <Table>
+      <Table data-testid="access-tokens-table">
         <TableHeader>
           <TableRow>
             <TableHead>Status</TableHead>
--- apps/web/tests/e2e/auth/list-user-access-tokens.spec.ts
+++ apps/web/tests/e2e/auth/list-user-access-tokens.spec.ts
@@ -1,5 +1,5 @@
 import { faker } from '@faker-js/faker'
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { makeAccessToken } from 'tests/api/make-access-token'
 import { resetAccessByUser } from 'tests/api/reset-access-by-user'
 
@@ -11,6 +11,7 @@ async function populateTable(email: string) {
     makeAccessToken(email, 'opera'),
     makeAccessToken(email, 'edge'),
     makeAccessToken(email, 'brave'),
+    makeAccessToken(email, 'chrome'),
   ])
 
   return response
@@ -29,7 +30,9 @@ test.describe('[ACCESS_TOKENS]', () => {
     await resetAccessByUser(email)
   })
 
-  test('should be able to list user access tokens', async ({ browser }) => {
+  test('should be able to list first page of access tokens', async ({
+    browser,
+  }) => {
     const browserContext = await browser.newContext()
 
     await browserContext.addCookies([
@@ -46,6 +49,63 @@ test.describe('[ACCESS_TOKENS]', () => {
     await page.goto('/', {
       waitUntil: 'networkidle',
     })
+
+    const rows = await page.locator(
+      '[data-testid="access-tokens-table"] tbody tr',
+    )
+
+    const rowCount = await rows.count()
+
+    expect(rowCount).toBe(5)
+
+    await page.waitForTimeout(1000)
+  })
+
+  test('should be able to navigate between pages', async ({ browser }) => {
+    const browserContext = await browser.newContext()
+
+    await browserContext.addCookies([
+      {
+        name: 'token',
+        value: token,
+        domain: 'localhost',
+        path: '/',
+      },
+    ])
+
+    const page = await browserContext.newPage()
+
+    await page.goto('/', {
+      waitUntil: 'networkidle',
+    })
+
+    await expect(page.getByTestId('previous-page-button').nth(0)).toBeDisabled()
+
+    await page.getByTestId('next-page-button').click()
+
+    const secondPageRows = await page.locator(
+      '[data-testid="access-tokens-table"] tbody tr',
+    )
+
+    const secondPageRowCount = await secondPageRows.count()
+
+    expect(secondPageRowCount).toBe(2)
+
+    await expect(page.getByTestId('previous-page-button').nth(0)).toBeEnabled()
+
+    await page.getByTestId('previous-page-button').click()
+
+    const firstPageRows = await page.locator(
+      '[data-testid="access-tokens-table"] tbody tr',
+    )
+
+    const firstPageRowCount = await firstPageRows.count()
+
+    expect(firstPageRowCount).toBe(5)
+
+    await expect(page.getByTestId('previous-page-button').nth(0)).toBeDisabled()
+
+    await expect(page.getByTestId('next-page-button').nth(0)).toBeEnabled()
 
     await page.waitForTimeout(1000)
   })
